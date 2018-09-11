cf63882368d3a222bc2c75756394b878b291e99c
Akabo027 <unkabore@gmail.com>
2018-09-11 18:35:22 -0400

    Arthurrrrrrrrrrrrrrrrrrrrrr

--- PersonInfo2.java
+++ PersonInfo2.java
@@ -1,9 +1,9 @@
 public class PersonInfo2 {
 
-    public static String firstName = "Allan";
-    public static String lastName = "Picard";
-    public static int studentNo = 35534;
-    public static String description = "MacBook";
+    public static String firstName = "Arthur";
+    public static String lastName = "Kabore";
+    public static int studentNo = 8530422;
+    public static String description = "Windows";
 
     public static void displayFr() {
         String out = SIConstants.GREETING_FR;
