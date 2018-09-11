786c403eb5ec2db28838776bf3a6232713d19d6a
mtrem122 <39308448+mtrem122@users.noreply.github.com>
2018-09-11 17:49:14 -0400

    Add files via upload

---
+++ PersonInfo1.java
@@ -0,0 +1,28 @@
+public class PersonInfo1 {
+
+    public static String firstName = "Marc-Frederick";
+    public static String lastName = "Tremblay";
+    public static int studentNo = 8603273;
+    public static String description = "Likes to code";
+
+    public static void displayFr() {
+        String out = SIConstants.GREETING_FR;
+        out += " " + firstName + " " + lastName + '.';
+        out += SIConstants.STUDENT_NUMBER_FR + studentNo + '.';
+        out += SIConstants.DESCRIPTION_FR + description + '.';
+        out += SIConstants.EOL;
+        
+        System.out.println(out);
+    }
+    
+    
+    public static void displayEn() {
+        String out = SIConstants.GREETING_EN;
+        out += " " + firstName + " " + lastName + '.';
+        out += SIConstants.STUDENT_NUMBER_EN + studentNo + '.';
+        out += SIConstants.DESCRIPTION_EN + description + '.';
+        out += SIConstants.EOL;
+        
+        System.out.println(out);
+    }
+}
